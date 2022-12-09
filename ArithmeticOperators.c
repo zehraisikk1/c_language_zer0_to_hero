9e9d33103d5a06dcc792428bf831eef8859998a4
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-09 21:28:18 +0300

    Create ArithmeticOperators.c

---
+++ ArithmeticOperators.c
@@ -0,0 +1,23 @@
+
+
+// Eksiklikleri gider, hatayi coz. 
+
+#include <stdio.h>
+int main()
+{
+    int a = 9, 
+        b = 4, 
+        c;
+    
+    c = a+b;
+    printf("a+b = %d \n",c);
+    c = a-b;
+    printf("a-b = %d \n",c);
+    c = 
+    printf("a*b = %d \n",c);
+    c = a/b;
+    printf("a/b = %d \n",c);
+    c = 
+    printf("Remainder when a divided by b = %d \n",c);
+    
+    return 0;
