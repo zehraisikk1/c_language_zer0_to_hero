a052cb0f11c600e700ef19b08386f519e81e7cb5
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-09 20:39:54 +0300

    Kalyonucgüller
    Ad,soyad,yaşı iki ile çarp

--- soyad_yas.c
+++ soyad_yas.c
@@ -1,2 +1,17 @@
 // Soyadini ve yasini yaz. 
 //   yasini 2 ile carp
+#include <stdio.h>
+
+ int main(){
+	char name[10] = "Güller";
+	char surname[10] = "Kalyoncu";
+	int age = 20;
+	age *= 2;
+
+	printf("Isim : %s \n",name);	
+	printf("Soyisim : %s \n",surname);   
+	printf("Age : %d \n",age);
+	
+	return 0;
+	
+ }
