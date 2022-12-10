64e9da57395e7ad3812ddd68589cf84c4d81ebaa
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-11 02:34:18 +0300

    Create hexaDec.c

---
+++ hexaDec.c
@@ -0,0 +1,11 @@
+
+
+
+int main()  
+{  
+  int y=0xA;  
+  printf("Hexadecimal value of y is: %x", y);  
+  printf("\nHexadecimal value of y is: %X",y);  
+  printf("\nInteger value of y is: %d",y);  
+    return 0;  
+}  
