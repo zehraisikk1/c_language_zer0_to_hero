fd6359ae8ae59bf73f548e98556c6c83292266f2
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-11 02:29:40 +0300

    Create Iki_sayi.c

---
+++ Cozumler/Iki_sayi.c
@@ -0,0 +1,16 @@
+#include<stdio.h>   
+
+int main()
+{    
+ int x = 0, y = 0,sonuc=0;  //Onceden deklare ettim. burada int veri tipi, x y degisken 
+  
+printf("Enter first number:");   //yazar
+scanf("%d",&x);  
+printf("Enter second number:");  //input alir
+scanf("%d",&y);  
+  
+sonuc=x+y;  
+printf("sum of 2 numbers:%d ",result);  
+  
+return 0;  
+}   
