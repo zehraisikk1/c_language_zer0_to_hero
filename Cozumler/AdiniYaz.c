d481bdfb735aa029c9bddf3602a5576c4f21442d
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-10 08:13:19 +0300

    Create AdiniYaz.c

---
+++ Cozumler/AdiniYaz.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+ int main()  
+  {
+     printf("Name   : Akif CIFCI"); 
+ 
+     return(0); 
+  }
+
+
+
+
+// Eger adini ekrandan input olarak almak istersen
+
+// #include <stdio.h>
+  
+// int main()
+// {
+//     char name[20];
+//     printf("Enter name: ");
+  
+//     // user input will be taken here
+//     scanf("%s", name);
+//     printf("Your name is %s.", name);
+//     return 0;
+// }
+// calsitirmadan yorumlu deaktif et
