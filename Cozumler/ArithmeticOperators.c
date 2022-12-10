c72333e322347ea303b13aac74b1355bb7cdd8c8
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-10 08:16:13 +0300

    Update ArithmeticOperators.c

--- Cozumler/ArithmeticOperators.c
+++ Cozumler/ArithmeticOperators.c
@@ -1,1 +1,21 @@
 
+
+#include <stdio.h>
+int main()
+{
+    int a = 10, b = 5;
+    int c;
+    
+    c = a+b;
+    printf("a+b = %d \n",c);
+    c = a-b;
+    printf("a-b = %d \n",c);
+    c = a*b;
+    printf("a*b = %d \n",c);
+    c = a/b;
+    printf("a/b = %d \n",c);
+    c = a%b;
+    printf("a b ye boldungunde kalan = %d \n",c);
+    
+    return 0;
+}
