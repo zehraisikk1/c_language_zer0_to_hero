88fb72aaf8a7be70c1827077aae51fc71cd5f957
Zehra Işık <74448702+zehraisikk1@users.noreply.github.com>
2022-12-11 16:32:48 +0300

    Zehra Işık

--- AdiniYaz.c
+++ AdiniYaz.c
@@ -1,1 +1,19 @@
 // BU programda adinizi yaziniz
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+   char name[10];
+   char surname[10];
+   
+  strcpy(name,"zehra");
+  strcpy(surname,"isik");
+  
+  printf("name :%s \n surname :%s \n",name,surname);
+
+  
+  
+   
+	return 0;
+	
+}
